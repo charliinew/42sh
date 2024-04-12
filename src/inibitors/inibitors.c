712955f9303c3f0e65ee9313479e149e84a48511
Dufour-Matisse <matisseduf@gmail.com>
2024-04-12 15:46:25 +0200

    Feat: string creations

--- src/inibitors/inibitors.c
+++ src/inibitors/inibitors.c
@@ -6,7 +6,44 @@
 */
 
 #include "minishell.h"
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
+
+static void redo_index(token_t **head)
+{
+
+}
+
+static void delete_node_string(token_t **head)
+{
+    token_t *next = 0;
+    token_t *current = (*head)->next;
+
+    for (; current->sep != '\"'; current = next) {
+        if (current->arg)
+            free(current->arg);
+        next = current->next;
+        free(current);
+    }
+    (*head)->next = next;
+    free(current);
+    if (next != NULL)
+        next->prev = *head;
+}
+
+static void fill_string(token_t **head)
+{
+    token_t *fill = *head;
+
+    for (token_t *current = fill->next; current->sep != '\"';
+    current = current->next) {
+        if (current->sep)
+            strncat(fill->arg, &current->sep, 1);
+        else
+            strcat(fill->arg, current->arg);
+    }
+}
 
 int get_string(token_t **head)
 {
@@ -21,5 +58,11 @@ int get_string(token_t **head)
     for (current = (*head)->next; current->sep != '\"';
     current = current->next)
         len += (current->sep == 0) ? strlen(current->arg) : 1;
+    (*head)->sep = 0;
+    free((*head)->arg);
+    (*head)->arg = malloc(len + 1);
+    (*head)->arg[0] = '\0';
+    fill_string(head);
+    delete_node_string(head);
     return 0;
 }
