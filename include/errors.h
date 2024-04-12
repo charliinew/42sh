b56f07010b67fa11da18f5d02736fd540d889873
Dufour-Matisse <matisseduf@gmail.com>
2024-04-12 12:14:38 +0200

    add: errors header meant to contain 42sh error messages

---
+++ include/errors.h
@@ -0,0 +1,10 @@
+/*
+** EPITECH PROJECT, 2024
+** B-PSU-200-LIL-2-1-42sh-nicolas.dumetz
+** File description:
+** Contain errors message used in 42sh
+*/
+
+#ifndef ERROR_MESSAGES_SH
+    #define ERROR_MESSAGES_SH
+#endif
